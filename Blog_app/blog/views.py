c434fa17a0f3e925d80fff62eeea0d3b1f12e531
developeryuldashev <yuldashevxolms321@gmail.com>
2021-12-06 19:56:19 +0500

    .

--- Blog_app/blog/views.py
+++ Blog_app/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DeleteView
 from .models import Post
+from django.views.generic.edit import CreateView
 
 
 class BlogListView(ListView):
@@ -9,4 +10,7 @@ class BlogListView(ListView):
 class BlogDetailView(DeleteView):
     model = Post
     template_name = 'post_detail.html'
-
+class BlogCreateView(CreateView):
+    model=Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
